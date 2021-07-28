b7f109638ad59ab8b7274343b5cf9c11db378dbc
chsneha-dev <chennamsneha0@gmail.com>
2021-07-29 01:38:18 +0530

    first commit

---
+++ comm/sneha/controller/ABCD.java
@@ -0,0 +1,11 @@
+package comm.sneha.controller;
+
+public class ABCD {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello world");
+	}
+
+}
---
+++ comm/sneha/controller/bcd.java
@@ -0,0 +1,10 @@
+package comm.sneha.controller;
+
+public class bcd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("bcd");
+	}
+
+}
